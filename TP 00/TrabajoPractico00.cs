885b1617a8e83e520784f482ea544613e88ebd16
Axel <alexaragonpregot17@gmail.com>
2026-04-07 20:52:39 -0300

    update

--- TP 00/TrabajoPractico00.cs
+++ TP 00/TrabajoPractico00.cs
@@ -7,3 +7,36 @@ class Program
         Console.WriteLine("Mi mundo,Axel");
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola, mi nombre es Axel");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Tengo 20 años");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Vivo en Argentina");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Este es mi primer programa");
+        Console.WriteLine("Estoy aprendiendo C#");
+    }
+}
